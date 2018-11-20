0bc03a10836d7cf319b198251cdcb7544d7685c2
Arvin-Mark <Arvin-Mark@outlive.com>
2018-11-20 21:05:08 +0800

    init

---
+++ common/config/params.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'support@example.com',
+    'user.passwordResetTokenExpire' => 3600,
+    'appVersion' => '1.0.0',
+    'appName' => '兴隆县纪委数据系统',
+    'appNameMini' => '兴隆',
+    'homePage' => '/'
+];
